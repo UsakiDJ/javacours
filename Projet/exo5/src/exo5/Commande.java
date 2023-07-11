61277d1e41cc0b9eb5494dbf4944d53458a19342
DRIDI Jessim <usakijess@gmail.com>
2023-07-11 17:29:43 +0200

    Ajout Exo POO

--- Projet/exo5/src/exo5/Commande.java
+++ Projet/exo5/src/exo5/Commande.java
@@ -1,5 +1,17 @@
 package exo5;
+import exo5.Article;
+import exo5.Client;
+import java.util.Date;
 
 public class Commande {
-
+		int numCommande;
+		Date date;
+		Client qui ;
+		Article tab[];
+		int qut[] ; 
+		
+		Commande(Article tab[])
+		{
+			this.tab= tab;
+		};
 }
